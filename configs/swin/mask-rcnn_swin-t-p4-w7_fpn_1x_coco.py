add3dd4e7b6f741a661623bb49265b4b5a75a524
SDUzry <2742264623@qq.com>
2024-07-19 10:10:42 +0800

    模型配置文件（分类，检测，分割）

--- configs/swin/mask-rcnn_swin-t-p4-w7_fpn_1x_coco.py
+++ configs/swin/mask-rcnn_swin-t-p4-w7_fpn_1x_coco.py
@@ -24,9 +24,56 @@ model = dict(
         with_cp=False,
         convert_weights=True,
         init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
-    neck=dict(in_channels=[96, 192, 384, 768]))
+    fc_head=dict( # 分类头
+        type='FcHead',
+        num_fcs=1,
+        in_channels=256,
+        fc_channels=512,
+        out_channels=256,
+        norm_cfg=dict(type='LN'),
+        act_cfg=dict(type='ReLU'),
+        num_classes=7, # 需要修改对应实际的数据
+        loss_cls=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        ),
+    neck=dict(in_channels=[96, 192, 384, 768]),
+    roi_head=dict(
+        _delete_=True,
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict( # 检测和分割头
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=10, # 检测类别
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+        mask_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        mask_head=dict(
+            type='FCNMaskHead',
+            num_convs=4,
+            in_channels=256,
+            conv_out_channels=256,
+            num_classes=10, # 分割类别
+            loss_mask=dict(
+                type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))),
+)
 
-max_epochs = 12
+max_epochs = 501
 train_cfg = dict(max_epochs=max_epochs)
 
 # learning rate
